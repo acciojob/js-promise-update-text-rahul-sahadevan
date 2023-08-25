f82353f5e3a010ae36b8968493023d8bf51c63cf
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-08-25 10:51:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const str = document.getElementById("output");
+
+let p = new Promise((resolve) =>{
+	setTimeout(() =>{
+		resolve("Hello, world!");
+	},1000)
+})
+
+let out = p.then((data) =>{
+	str.innerText = data;
+})
